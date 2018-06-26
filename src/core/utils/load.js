cbf35c522e384f93abc11dc6da3b513b198fb84a
Renato Oda <renato.oda2@gmail.com>
2018-06-26 13:55:04 -0300

    Fixed load

--- src/core/utils/load.js
+++ src/core/utils/load.js
@@ -4,18 +4,12 @@ const dotenv = require('dotenv');
 
 dotenv.load({ silent: true });
 
-const getServer = () => {
-  return {
-    host: process.env.HOST || process.env['SERVER_HOST'],
-    port: process.env.PORT || process.env['SERVER_PORT'] || '4000'
-  };
-};
+const getServer = () => ({
+  host: process.env.HOST || process.env['SERVER_HOST'],
+  port: process.env.PORT || process.env['SERVER_PORT'] || '4000'
+});
 
-const getDatabase = () => {
-  return {
-    host: process.env.DATABASE || process.env['DATABASE'] || 'mongodb://localhost:27017'
-  };
-};
+const getDatabase = () => ({ host: process.env.DATABASE || process.env['DATABASE'] || 'mongodb://localhost:27017' });
 
 module.exports = {
   getServer,
